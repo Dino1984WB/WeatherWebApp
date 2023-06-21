f9ea3207b11228d1dfa9a582dc8be4330d4f6262
Dino1984WB <williamkbukowkidev@gmail.com>
2023-06-21 19:29:26 -0400

    added comments and new frame

--- ui.js
+++ ui.js
@@ -1,11 +1,10 @@
 class UI {
-    static updateUI(weatherData) {
-      const weatherContainer = document.getElementById('weatherContainer');
-      weatherContainer.innerHTML = `
-        <h2>${weatherData.location.name}</h2>
-        <p>Temperature: ${weatherData.current.temp_c}°C</p>
-        <p>Condition: ${weatherData.current.condition.text}</p>
-      `;
-    }
+  static updateUI(weatherData) {
+    const weatherContainer = document.getElementById('weatherContainer');
+    weatherContainer.innerHTML = `
+      <h2>${weatherData.location.name}</h2>
+      <p>Temperature: ${weatherData.current.temp_f}°F</p>
+      <p>Condition: ${weatherData.current.condition.text}</p>
+    `;
   }
-  
+}
--- weather.js
+++ weather.js
@@ -1,13 +1,12 @@
 class Weather {
-    constructor() {
-      this.apiKey = '34e1cd8db4fc460094c173346231806';
-    }
-  
-    // Fetch weather data from API
-    async getWeatherData(location) {
-      const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${location}`);
-      const data = await response.json();
-      return data;
-    }
+  constructor() {
+    this.apiKey = 'YOUR_API_KEY';
   }
-  
+
+  // Fetch weather data from API
+  async getWeatherData(location) {
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${location}`);
+    const data = await response.json();
+    return data;
+  }
+}

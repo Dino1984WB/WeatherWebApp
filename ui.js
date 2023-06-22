09fcc975b2ecbe679b66cff8a31d29fb7f106ad0
Dino1984WB <williamkbukowkidev@gmail.com>
2023-06-21 20:01:14 -0400

    haha added ascii art

--- ui.js
+++ ui.js
@@ -1,12 +1,47 @@
 class UI {
   static updateUI(weatherData) {
-    console.log(weatherData); // Log the weatherData object to inspect its structure
-    
     const weatherContainer = document.getElementById('weatherContainer');
     weatherContainer.innerHTML = `
       <h2>${weatherData.location.name}</h2>
       <p>Temperature: ${weatherData.current.temp_f}°F</p>
       <p>Condition: ${weatherData.current.condition.text}</p>
     `;
+
+    const weatherAnimation = document.getElementById('weatherAnimation');
+    const condition = weatherData.current.condition.text.toLowerCase();
+    const asciiAnimation = this.getAsciiAnimation(condition);
+    weatherAnimation.innerHTML = asciiAnimation;
+  }
+
+  static getAsciiAnimation(condition) {
+    switch (condition) {
+      case 'partly cloudy':
+        return `
+          .--.
+       .'_\/_'.
+      '. /--\ .'
+       ' |  | '
+         '.'
+        `;
+      case 'sunny':
+        return `
+         \\   /
+          .-.
+      --: / | \\ :--
+          || ||
+        `;
+      case 'windy':
+        return `
+        _  _
+        ( '  ' )
+       (    )    ')
+        (_ (_ .  _) _)
+          '~~'\~~'
+              '\~~
+                \_
+        `;
+      default:
+        return '';
+    }
   }
 }
